3b1bb79c5ec48de0c0d8b54e5acc8a8788a188d7
yusin-bit <yujeonghyeon345@gmail.com>
2026-08-21 01:46:36 +0900

    Add src/service/ProFileServiceImpl.java

---
+++ src/service/ProFileServiceImpl.java
@@ -0,0 +1,45 @@
+package service;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import dto.ProFileDto;
+import exception.ProfileAlreadyExistsException;
+import exception.ProfileNotFoundException;
+
+public class ProFileServiceImpl implements ProFileService {
+
+	@Override
+	public void saveprofile(ProFileDto profiledto) throws IOException, ProfileAlreadyExistsException {
+		String name = profiledto.getName();
+		File file = new File(name + ".txt");
+		if (file.exists()) {
+			throw new ProfileAlreadyExistsException("이미 저장된 프로필입니다");
+		}
+
+		int weight = profiledto.getWeight();
+		int password = profiledto.getPassword();
+		String str = weight + ":" + password;
+		try (FileWriter fw = new FileWriter(file)) {
+			fw.write(str); 
+		}
+	}
+
+	@Override
+	public ProFileDto loadprofile(String name) throws IOException, ProfileNotFoundException {
+		File file = new File(name + ".txt");
+		if (file.exists()) {
+			try (FileReader fr = new FileReader(file);
+					BufferedReader br = new BufferedReader(fr)) {
+				String readline = br.readLine();
+				String[] data = readline.split(":");
+				int weight = Integer.parseInt(data[0]);
+				int password = Integer.parseInt(data[1]);
+				return new ProFileDto(name, weight, password);
+			}
+		}
+		throw new ProfileNotFoundException("파일을 찾을 수 없습니다");
+	}
+}
