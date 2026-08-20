442ee3496d2fb8336c036ea6e9960d40249589ed
yusin-bit <yujeonghyeon345@gmail.com>
2026-08-21 01:46:35 +0900

    Add src/service/ProFileService.java

---
+++ src/service/ProFileService.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.io.IOException;
+
+import dto.ProFileDto;
+import exception.ProfileAlreadyExistsException;
+import exception.ProfileNotFoundException;
+
+public interface ProFileService {
+
+	void saveprofile(ProFileDto profiledto) throws IOException, ProfileAlreadyExistsException;
+
+	ProFileDto loadprofile(String name) throws IOException, ProfileNotFoundException;
+}
